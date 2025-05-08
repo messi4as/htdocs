6220ce30dae90a4812a2c71eb8fdbcfe71690efd
messi4as <messias.meme@gmail.com>
2025-05-08 11:56:47 -0300

    Teste

--- teste.php
+++ teste.php
@@ -1,8 +1,4 @@
 <?php
-<<<<<<< HEAD
-$nome = "Teste3";
-=======
-$nome = "Teste2";
->>>>>>> 1d21502107c2b477e9bdca9c96fe9cac4da9a80e
+$nome = "Teste4";
 echo $nome; // Defina um ponto de interrupção nesta linha
 ?>
